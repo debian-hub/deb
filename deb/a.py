cccf580c54dd5378fd604d97f8250423a2ab729c
jakbin <jakbin4747@gmail.com>
2021-09-16 14:45:09 +0530

    try except, install deb file, delete after installation

--- deb/a.py
+++ deb/a.py
@@ -2,8 +2,7 @@ __version__ = "1.0.0"
 
 import argparse
 import os
-# import re
-# from click import confirm
+from click import confirm
 import sqlite3
 import requests
 import json
@@ -62,7 +61,11 @@ def main(cwd=None):
 
 
 	if (list):
-		pkgs = cursor.execute("SELECT name,version,architecture FROM pkgs ORDER BY name").fetchall()
+		try:
+			pkgs = cursor.execute("SELECT name,version,architecture FROM pkgs ORDER BY name").fetchall()
+		except sqlite3.Error as e:
+			print(e)
+			raise
 		conn.close()
 		# print(pkgs)
 
@@ -76,8 +79,12 @@ def main(cwd=None):
 
 	elif (show):
 		for i in args.show:
-			pkgs = cursor.execute("SELECT name,version,architecture,description,section,maintainer,homePage FROM pkgs WHERE name = ?", (i,),).fetchall()
-			conn.close()
+			try:
+				pkgs = cursor.execute("SELECT name,version,architecture,description,section,maintainer,homePage FROM pkgs WHERE name = ?", (i,),).fetchall()
+			except sqlite3.Error as e:
+				print(e)
+				raise
+		conn.close()
 		# print(pkgs)
 		# print(type(pkgs))
 
@@ -97,24 +104,30 @@ def main(cwd=None):
 	elif (install):
 		print(args.i)
 		for i in args.i:
-			pkgs = cursor.execute("SELECT url FROM pkgs WHERE name = ?", (i,),).fetchall()
-			conn.close()
+			try:
+				pkgs = cursor.execute("SELECT url FROM pkgs WHERE name = ?", (i,),).fetchall()
+			except sqlite3.Error as e:
+				print(e)
+				raise
+		conn.close()
 		# print(pkgs)
 
 		for j in pkgs:
-			# r = requests.get(j[0])
 			print(j[0])
 			url = j[0]
 
 			home_path = Path.home()
-			sub_path = "tmp"
+			sub_path = "tmp/deb"
 
 			filesize = int(requests.head(url).headers["Content-Length"])
 			filename = os.path.basename(url)
-
-			os.makedirs(os.path.join(home_path, sub_path), exist_ok=True)
-
+			try:
+				os.makedirs(os.path.join(home_path, sub_path), exist_ok=True)
+			except OSError as error:
+				print(error)
+				raise
 			dl_path = os.path.join(home_path, sub_path, filename)
+			print(dl_path)
 			chunk_size = 1024
 
 
@@ -130,8 +143,16 @@ def main(cwd=None):
 			        datasize = f.write(chunk)
 			        progress.update(datasize)
 
-		# resp = json.loads(r.text)
-		# print(resp['browser_download_url'])
+			if os.geteuid() == 0:
+				os.system(f"dpkg -i {dl_path}") 
+			else:
+				os.system(f"sudo dpkg -i {dl_path}")
+
+			if confirm(f"Do you want delete {filename}", default=True):
+				try:
+					os.remove(dl_path)
+				except OSError as error:
+					print(error)
 
 	elif version:
 		print(__version__)
